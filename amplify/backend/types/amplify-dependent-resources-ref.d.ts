f15f056e99cdeb237a2fbdc07036883ddd2142cd
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-01-30 23:04:06 +0530

    amplify initialized

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "nafzen": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "NAFZen": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3nafzenstorage59214087": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
